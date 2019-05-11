69bdece4fba475fa1f0307437d2ce19ea8171c9f
Martin Bogusz <ga84zod2@mpp2-login5.cos.lrz.de>
2019-05-11 20:13:29 +0200

    added support for upcxx

--- src/examples/swe_upcxx.cpp
+++ src/examples/swe_upcxx.cpp
@@ -361,12 +361,12 @@ int main(int argc, char** argv) {
 	printf("Rank %i : Compute Time (CPU): %fs - (WALL): %fs | Total Time (Wall): %fs\n", myUpcxxRank, simulation.computeTime, simulation.computeTimeWall, wallTime);
 
     uint64_t  sumFlops = upcxx::reduce_all(simulation.getFlops(), upcxx::op_fast_add).wait();
-
+    if(myUpcxxRank == 0){
     std::cout   << "Rank: " << myUpcxxRank << std::endl
                 << "Flop count: " << sumFlops << std::endl
-                << "Flops(Total): " << ((float)sumFlops)/(wallTime*1000000000) << std::endl
+                << "Flops(Total): " << ((float)sumFlops)/(wallTime*1000000000) << "GFLOPS"<< std::endl
                 << "Flops(Single): "<< ((float)simulation.getFlops())/(wallTime*1000000000) << std::endl;
-
+}
     upcxx::finalize();
 
 	return 0;
