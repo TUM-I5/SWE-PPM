0c18284fc4ae2c4a0cf47ce94675e4e129570b00
martin <ga84zod@tum.de>
2020-04-06 21:15:38 +0200

    Test on Bug in Chameleon
    Signed-off-by: martin <ga84zod@tum.de>

--- src/blocks/SWE_Block.hh
+++ src/blocks/SWE_Block.hh
@@ -327,6 +327,8 @@ void SWE_Block<T, Buffer>::printLtsStats() {
 
 template<typename T, typename Buffer>
 bool SWE_Block<T, Buffer>::allGhostlayersInSync() {
+    std::cout <<  "allGhostlayersInSync "<< (receivedGhostlayer[BND_LEFT] != GL_UNVALID && receivedGhostlayer[BND_RIGHT] != GL_UNVALID
+    && receivedGhostlayer[BND_TOP] != GL_UNVALID && receivedGhostlayer[BND_BOTTOM] != GL_UNVALID) << std::endl;
     return receivedGhostlayer[BND_LEFT] != GL_UNVALID && receivedGhostlayer[BND_RIGHT] != GL_UNVALID
            && receivedGhostlayer[BND_TOP] != GL_UNVALID && receivedGhostlayer[BND_BOTTOM] != GL_UNVALID;
 }
