38ebf616c7b3f9140f0c04338583500d5596ddf4
martin <ga84zod@tum.de>
2020-06-16 13:52:46 +0200

    Fixed Bug in Computation for all frameworks
    Signed-off-by: martin <ga84zod@tum.de>

--- src/blocks/SWE_DimensionalSplittingHpx.cpp
+++ src/blocks/SWE_DimensionalSplittingHpx.cpp
@@ -321,14 +321,14 @@ void SWE_DimensionalSplittingHpx::computeNumericalFluxes() {
     if (!allGhostlayersInSync()) return;
 //maximum (linearized) wave speed within one iteration
     float maxWaveSpeed = (float) 0.;
-    float maxEdgeSpeed = 0;
+
     /***************************************************************************************
      * compute the net-updates for the vertical edges
      **************************************************************************************/
 
     for (int i = 1; i < nx+2; i++) {
         const int ny_end = ny+1;
-
+        float maxEdgeSpeed = 0;
 #if defined(VECTORIZE)
 
             // iterate over all rows, including ghost layer
@@ -357,7 +357,7 @@ void SWE_DimensionalSplittingHpx::computeNumericalFluxes() {
 
     for (int i=1; i < nx + 1; i++) {
         const int ny_end = ny+2;
-
+        float maxEdgeSpeed = 0;
 #if defined(VECTORIZE)
 
         // iterate over all rows, including ghost layer
