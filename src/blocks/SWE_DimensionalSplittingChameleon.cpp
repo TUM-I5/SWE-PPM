41194642bb962d7adf396f23282fbebebe55a4ee
martin <ga84zod@tum.de>
2020-06-17 18:48:49 +0200

    Fixed Bug in Computation for all frameworks
    Signed-off-by: martin <ga84zod@tum.de>

--- src/blocks/SWE_DimensionalSplittingChameleon.cpp
+++ src/blocks/SWE_DimensionalSplittingChameleon.cpp
@@ -627,9 +627,9 @@ void SWE_DimensionalSplittingChameleon::computeNumericalFluxes() {
     for (int i = 1; i < block->nx+1; i++) {
         for (int j = 1; j < block->ny + 1; j++) {
             if(block->h[i][j] != htest[i][j]) std::cout << block->h[i][j] <<"!= "<< htest[i][j] << " " << block->nx << " "<< block->ny << " " << block->h.getRows()<< std::endl;
-            block->getModifiableWaterHeight()[i][j] -=dt / block->dx * (block->hNetUpdatesRight[i - 1][j - 1] + block->hNetUpdatesLeft[i][j - 1]) + dt / block->dy * (block->hNetUpdatesAbove[i - 1][j - 1] + block->hNetUpdatesBelow[i - 1][j]);
-            block->getModifiableMomentumHorizontal()[i][j]  -= dt / block->dx * (block->huNetUpdatesRight[i - 1][j - 1] + block->huNetUpdatesLeft[i][j - 1]);
-            block->getModifiableMomentumVertical()[i][j]  -= dt / block->dy * (block->hvNetUpdatesAbove[i - 1][j - 1] + block->hvNetUpdatesBelow[i - 1][j]);
+            block->getModifiableWaterHeight()[i][j] =htest[i][j]-dt / block->dx * (block->hNetUpdatesRight[i - 1][j - 1] + block->hNetUpdatesLeft[i][j - 1]) + dt / block->dy * (block->hNetUpdatesAbove[i - 1][j - 1] + block->hNetUpdatesBelow[i - 1][j]);
+            block->getModifiableMomentumHorizontal()[i][j]  =hutest[i][j]- dt / block->dx * (block->huNetUpdatesRight[i - 1][j - 1] + block->huNetUpdatesLeft[i][j - 1]);
+            block->getModifiableMomentumVertical()[i][j]  =hvtest[i][j]- / block->dy * (block->hvNetUpdatesAbove[i - 1][j - 1] + block->hvNetUpdatesBelow[i - 1][j]);
 
             if ( block->getModifiableWaterHeight()[i][j] < 0) {
                 //TODO: dryTol
@@ -672,9 +672,9 @@ void SWE_DimensionalSplittingChameleon::updateUnknowns (float dt) {
 
     args[11] = chameleon_map_data_entry_create(this->hvNetUpdatesBelow.getRawPointer(), sizeof(float)*(nx + 1)*(ny + 2), CHAM_OMP_TGT_MAPTYPE_TO);
     args[12] = chameleon_map_data_entry_create(this->hvNetUpdatesAbove.getRawPointer(), sizeof(float)*(nx + 1)*(ny + 2), CHAM_OMP_TGT_MAPTYPE_TO);
-    args[13] = chameleon_map_data_entry_create(this->getWaterHeight().getRawPointer(), sizeof(float)*(nx + 2)*(ny + 2), CHAM_OMP_TGT_MAPTYPE_ALWAYS);
-    args[14] = chameleon_map_data_entry_create(this->hu.getRawPointer(), sizeof(float)*(nx + 2)*(ny + 2), CHAM_OMP_TGT_MAPTYPE_ALWAYS);
-    args[15] = chameleon_map_data_entry_create(this->hv.getRawPointer(), sizeof(float)*(nx + 2)*(ny + 2), CHAM_OMP_TGT_MAPTYPE_ALWAYS);
+    args[13] = chameleon_map_data_entry_create(this->getWaterHeight().getRawPointer(), sizeof(float)*(nx + 2)*(ny + 2), CHAM_OMP_TGT_MAPTYPE_FROM);
+    args[14] = chameleon_map_data_entry_create(this->hu.getRawPointer(), sizeof(float)*(nx + 2)*(ny + 2), CHAM_OMP_TGT_MAPTYPE_FROM);
+    args[15] = chameleon_map_data_entry_create(this->hv.getRawPointer(), sizeof(float)*(nx + 2)*(ny + 2), CHAM_OMP_TGT_MAPTYPE_FROM);
 
     cham_migratable_task_t *cur_task = chameleon_create_task(
             (void *)&updateUnkownsKernel,
