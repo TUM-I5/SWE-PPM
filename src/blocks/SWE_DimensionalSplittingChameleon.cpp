b40199798c64fca72a33be915c00f88eda1cfdb9
martin <ga84zod@tum.de>
2020-06-17 17:01:09 +0200

    Fixed Bug in Computation for all frameworks
    Signed-off-by: martin <ga84zod@tum.de>

--- src/blocks/SWE_DimensionalSplittingChameleon.cpp
+++ src/blocks/SWE_DimensionalSplittingChameleon.cpp
@@ -564,64 +564,7 @@ void computeNumericalFluxesKernel(SWE_DimensionalSplittingChameleon* block, floa
 
 
 void SWE_DimensionalSplittingChameleon::computeNumericalFluxes() {
-    if (!allGhostlayersInSync()) return;
-//maximum (linearized) wave speed within one iteration
-    float maxWaveSpeed = (float) 0.;
 
-    /***************************************************************************************
-     * compute the net-updates for the vertical edges
-     **************************************************************************************/
-
-    for (int i = 1; i < nx+2; i++) {
-        for (int j=1; j < ny+1; ++j) {
-            float maxEdgeSpeed;
-
-            solver.computeNetUpdates (
-                    h[i - 1][j], h[i][j],
-                    hu[i - 1][j], hu[i][j],
-                    b[i - 1][j], b[i][j],
-                    hNetUpdatesLeft[i - 1][j - 1], hNetUpdatesRight[i - 1][j - 1],
-                    huNetUpdatesLeft[i - 1][j - 1], huNetUpdatesRight[i - 1][j - 1],
-                    maxEdgeSpeed
-            );
-
-            //update the thread-local maximum wave speed
-            maxWaveSpeed = std::max(maxWaveSpeed, maxEdgeSpeed);
-        }
-    }
-
-    /***************************************************************************************
-     * compute the net-updates for the horizontal edges
-     **************************************************************************************/
-
-    for (int i=1; i < nx + 1; i++) {
-        for (int j=1; j < ny + 2; j++) {
-            float maxEdgeSpeed;
-
-            solver.computeNetUpdates (
-                    h[i][j - 1], h[i][j],
-                    hv[i][j - 1], hv[i][j],
-                    b[i][j - 1], b[i][j],
-                    hNetUpdatesBelow[i - 1][j - 1], hNetUpdatesAbove[i - 1][j - 1],
-                    hvNetUpdatesBelow[i - 1][j - 1], hvNetUpdatesAbove[i - 1][j - 1],
-                    maxEdgeSpeed
-            );
-
-            //update the maximum wave speed
-            maxWaveSpeed = std::max (maxWaveSpeed, maxEdgeSpeed);
-        }
-    }
-
-    if (maxWaveSpeed > 0.00001) {
-
-        maxTimestep = std::min (dx / maxWaveSpeed, dy / maxWaveSpeed);
-
-        maxTimestep *= (float) .4; //CFL-number = .5
-    } else {
-        //might happen in dry cells
-        maxTimestep = std::numeric_limits<float>::max ();
-    }
-    return;
     if (!allGhostlayersInSync()) return;
     collector.addFlops(2*135*nx*ny);
 
