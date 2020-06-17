f32ea9b2744ffe243e679437e1a58a98ebc5187b
martin <ga84zod@tum.de>
2020-06-17 22:00:41 +0200

    Fixed Bug in Computation for all frameworks
    Signed-off-by: martin <ga84zod@tum.de>

--- src/blocks/SWE_DimensionalSplittingHpx.cpp
+++ src/blocks/SWE_DimensionalSplittingHpx.cpp
@@ -419,7 +419,7 @@ void SWE_DimensionalSplittingHpx::updateUnknowns(float dt) {
 #if defined(VECTORIZE)
 
         // iterate over all rows, including ghost layer
-#pragma omp simd
+//#pragma omp simd
 #endif // VECTORIZE
 
         for (int j = 1; j < ny_end; j++) {
