ab4fd67b6b8f55f5b67541196880a1afc53cb974
martin <ga84zod@tum.de>
2020-06-23 14:44:08 +0200

    Added synchronization to chameleon
    Signed-off-by: martin <ga84zod@tum.de>

--- src/blocks/SWE_DimensionalSplittingHpx.cpp
+++ src/blocks/SWE_DimensionalSplittingHpx.cpp
@@ -354,14 +354,14 @@ void SWE_DimensionalSplittingHpx::computeNumericalFluxes() {
     /***************************************************************************************
      * compute the net-updates for the horizontal edges
      **************************************************************************************/
-    float maxTestSpeed = 0;
+
     for (int i=1; i < nx + 1; i++) {
         const int ny_end = ny+2;
 
 #if defined(VECTORIZE)
 
         // iterate over all rows, including ghost layer
-#pragma omp simd reduction(max:maxWaveSpeed)
+#pragma omp simd reduction(max:maxWaveSpeed) lastprivate(maxEdgeSpeed)
 #endif // VECTORIZE
         for (int j=1; j < ny_end; ++j) {
 
@@ -375,7 +375,7 @@ void SWE_DimensionalSplittingHpx::computeNumericalFluxes() {
             );
 
             //update the maximum wave speed
-           // maxWaveSpeed = std::max (maxWaveSpeed, maxEdgeSpeed);
+           maxWaveSpeed = std::max (maxWaveSpeed, maxEdgeSpeed);
            //maxTestSpeed = std::max (maxTestSpeed, maxEdgeSpeed);
 
         }
