b322e35b33f9fc42e58dca9ca1f6436132bfc704
martin <ga84zod@tum.de>
2020-06-24 21:10:00 +0200

    Added vectorization to chameleon modified colletor for all frameworks
    Signed-off-by: martin <ga84zod@tum.de>

--- BuildChameleon.cmake
+++ BuildChameleon.cmake
@@ -1,4 +1,11 @@
 find_package(MPI REQUIRED)
+find_library(CHAMELEON_LIB CHAMELEON HINTS "$ENV{CHAM_PATH}/lib")
+
+list(APPEND chameleon_include_directories $ENV{CHAM_PATH}/include)
+
+list(APPEND chameleon_link_libraries ${CHAMELEON_LIB})
+
+
 list(APPEND chameleon_include_directories ${MPI_CXX_INCLUDE_PATH})
 list(APPEND chameleon_compile_options ${MPI_CXX_COMPILE_FLAGS})
 list(APPEND chameleon_link_libraries ${MPI_CXX_LIBRARIES} ${MPI_CXX_LINK_FLAGS})
