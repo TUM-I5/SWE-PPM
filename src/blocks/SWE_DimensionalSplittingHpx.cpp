42271981f66206b5e7646e7c3cbd4a43b1b68414
martin <ga84zod@tum.de>
2020-06-29 14:16:13 +0200

    Added little debug message
    Signed-off-by: martin <ga84zod@tum.de>

--- src/blocks/SWE_DimensionalSplittingHpx.cpp
+++ src/blocks/SWE_DimensionalSplittingHpx.cpp
@@ -399,7 +399,7 @@ void SWE_DimensionalSplittingHpx::computeNumericalFluxes() {
     if (localTimestepping) {
         maxTimestep = getRoundTimestep(maxTimestep);
     }
-    if(maxTimestep< 0 || maxTimestep > 1  )std::cout << maxTimestep <<" something went wrong!\n";
+    if(maxTimestep<0.f ||maxTimestep > 1.f  )std::cout << maxTimestep <<" something went wrong!\n";
     collector.addTimestep(maxTimestep);
 
 
