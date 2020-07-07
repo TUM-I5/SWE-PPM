2ba6235058568629a8206ffb83891d5af3035b1b
martin <ga84zod@tum.de>
2020-07-07 21:54:46 +0200

    Added loadbalancer to charm
    Signed-off-by: martin <ga84zod@tum.de>

--- src/blocks/SWE_DimensionalSplittingCharm.cpp
+++ src/blocks/SWE_DimensionalSplittingCharm.cpp
@@ -95,7 +95,7 @@ SWE_DimensionalSplittingCharm::SWE_DimensionalSplittingCharm(int nx, int ny, flo
 SWE_DimensionalSplittingCharm::~SWE_DimensionalSplittingCharm() {}
 void SWE_DimensionalSplittingCharm::ResumeFromSync() {
     CkPrintf("I'm chare %d, I moved to PE %d \n", thisIndex, CkMyPe());
-    compute();
+    //compute();
 }
 
 void SWE_DimensionalSplittingCharm::xSweep() {
