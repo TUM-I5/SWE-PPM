8e494a2bb5032b28f7e2337dd189714d7adaca79
martin <ga84zod@tum.de>
2020-07-07 14:25:33 +0200

    Added loadbalancer to charm
    Signed-off-by: martin <ga84zod@tum.de>

--- src/blocks/SWE_DimensionalSplittingCharm.hh
+++ src/blocks/SWE_DimensionalSplittingCharm.hh
@@ -105,7 +105,7 @@ public:
         PUParray(p,collector->serialize(collectorSerializer),5);
 
         if (p.isUnpacking()){
-            CkPrintf("%d:Unpacking %d %d %f %f\n",myRank, nx,ny,dx,dy);
+            CkPrintf("Unpacking %d %d %f %f\n", nx,ny,dx,dy);
             // For the x-sweep
             hNetUpdatesLeft = Float2DNative(nx + 2, ny + 2);
             hNetUpdatesRight = Float2DNative(nx + 2, ny + 2);
