b440bbedf543c0e26e09ecb1f3c98b0052ca9b07
martin <ga84zod@tum.de>
2020-07-09 00:58:55 +0200

    Added loadbalancer to charm
    Signed-off-by: martin <ga84zod@tum.de>

--- src/tools/CollectorCharm.hpp
+++ src/tools/CollectorCharm.hpp
@@ -46,7 +46,7 @@ public:
 
                 instance.measure_ctrs[i] = std::chrono::steady_clock::time_point(std::chrono::milliseconds((long)arr[i+5]));
                 if(i==CTR_WALL)
-                    std::cout<<  "recv measure_ctr "<<(long)arr[i+5] << std::endl;
+                    std::cout<<  "recv measure_ctr "<<arr[i+5] << std::endl;
             }
         }
 
