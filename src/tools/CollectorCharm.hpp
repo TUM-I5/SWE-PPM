05b31ecc1a7b60ebecb2294fee0b33ae6f88ccc5
martin <ga84zod@tum.de>
2020-07-09 00:04:51 +0200

    Added loadbalancer to charm
    Signed-off-by: martin <ga84zod@tum.de>

--- src/tools/CollectorCharm.hpp
+++ src/tools/CollectorCharm.hpp
@@ -27,7 +27,7 @@ public:
         if(all){
             for (int i = 0; i < 4; i++) {
 
-                arr[i + 5] = std::chrono::time_point_cast<double>( measure_ctrs[i]);
+                arr[i + 5] = std::chrono::duration<double>(measure_ctrs[i].time_since_epoch());
             }
         }
 
