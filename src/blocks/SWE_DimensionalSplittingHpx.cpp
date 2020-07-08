3624e68c640ce49a25d3ddf0fee68f8f6c59fead
martin <ga84zod@tum.de>
2020-07-08 14:17:33 +0200

    Added loadbalancer to charm
    Signed-off-by: martin <ga84zod@tum.de>

--- src/blocks/SWE_DimensionalSplittingHpx.cpp
+++ src/blocks/SWE_DimensionalSplittingHpx.cpp
@@ -461,7 +461,14 @@ void SWE_DimensionalSplittingHpx::computeNumericalFluxes() {
 
     //collector.addTimestep(maxTimestep);
 
+      std::string hString="";
+      for (int i = 1; i < nx+2; i++) {
 
+            for (int j=1; j < ny+2; ++j) {
+                hString += std::to_string(h[i][j])+ " ";
+            }
+        }
+        CkPrintf("%d: %s\n",thisIndex, hString.c_str());
 }
 
 
