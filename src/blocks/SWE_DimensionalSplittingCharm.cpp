19b6ef1ac8045715da68834c011756ed211d8a3d
martin <ga84zod@tum.de>
2020-07-08 21:29:37 +0200

    Added loadbalancer to charm
    Signed-off-by: martin <ga84zod@tum.de>

--- src/blocks/SWE_DimensionalSplittingCharm.cpp
+++ src/blocks/SWE_DimensionalSplittingCharm.cpp
@@ -104,6 +104,11 @@ void SWE_DimensionalSplittingCharm::ResumeFromSync() {
     //CkPrintf("I'm chare %d, I moved to PE %d \n", thisIndex, CkMyPe());
     //compute();
     if (currentSimulationTime >= simulationDuration) {
+        double serialize[5];
+        collector->serialize(serialize);
+        CkPrintf("%d: Done! at %d \n",thisIndex,CkMyPe());
+
+        mainProxy.done(thisIndex,serialize[0],serialize[1],serialize[2],serialize[3],serialize[4]);
         contribute(CkCallback(CkReductionTarget(swe_charm, allEnd), mainProxy));
     }else {
         thisProxy[thisIndex].compute();
