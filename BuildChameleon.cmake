26cf3d92682778327d1b14d4c80e0ea23735fd2f
martin <ga84zod@tum.de>
2020-09-10 10:57:06 +0200

    Cleaned up repository a bit
    Signed-off-by: martin <ga84zod@tum.de>

--- BuildChameleon.cmake
+++ BuildChameleon.cmake
@@ -12,4 +12,4 @@ list(APPEND chameleon_compile_options ${MPI_CXX_COMPILE_FLAGS})
 list(APPEND chameleon_link_libraries ${MPI_CXX_LIBRARIES} ${MPI_CXX_LINK_FLAGS})
 
 set(BLOCK_FILES ${BLOCKS}/SWE_Block.hh ${BLOCKS}/SWE_DimensionalSplittingChameleon.hh ${BLOCKS}/SWE_DimensionalSplittingChameleon.cpp)
-set(EXAMPLE_FILES ${EXAMPLES}/swe_chameleon_new.cpp)
+set(EXAMPLE_FILES ${EXAMPLES}/swe_chameleon.cpp)
